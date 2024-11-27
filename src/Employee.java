db0f0d8046052da6a432bfa2f987ab0efbc11efe
StenographerSure <glibockiy@inbox.ru>
2024-11-27 23:38:16 +0400

    initial commit

---
+++ src/Employee.java
@@ -0,0 +1,78 @@
+import java.util.Objects;
+
+public class Employee {
+
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private int department;
+    private int salary;
+    private final int id;
+
+    public Employee(String lastName, String firstName, String middleName, int department, int salary) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        if (department >= 0 && department <= 5) {
+            this.department = department;
+        }
+        this.salary = salary;
+        this.id = Main.counterID;
+        Main.counterID++;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && Objects.equals(firstName, employee.firstName) && Objects.equals(middleName, employee.middleName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, middleName, lastName, department, salary);
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,72 @@
 public class Main {
+    public static int counterID = 0;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee[] employees = new Employee[10];
+        employees[counterID] = new Employee("Иванов", "Иван", "Денисович", 4, 40_000);
+        employees[counterID] = new Employee("Савельев", "Борис", "Владимирович", 1, 100_000);
+        employees[counterID] = new Employee("Сергеев", "Денис", "Владимирович", 1, 80_000);
+        employees[counterID] = new Employee("Турганова", "Елена", "Юрьевна", 2, 50_000);
+        employees[counterID] = new Employee("Ульянова", "Тамара", "Сергеевна", 2, 50_000);
+        employees[counterID] = new Employee("Тарасова", "Евгения", "Владиславовна", 3, 40_000);
+        employees[counterID] = new Employee("Сиренин", "Сергей", "Сергеевич", 5, 60_000);
+        employees[counterID] = new Employee("Камышов", "Иван", "Иванович", 5, 37_000);
+        employees[counterID] = new Employee("Попов", "Николай", "Потапович", 3, 40_000);
+        employees[counterID] = new Employee("Попов", "Виталий", "Артемьевич", 5, 45_000);
+
+//        getList(employees);
+//        System.out.println(calculateSalaries(employees));
+//        System.out.println(minSalary(employees));
+//        System.out.println(getAverageSalary(employees));
+//        printNames(employees);
     }
+
+    public static void getList(Employee[] employees) {
+        for (Employee person : employees) {
+            System.out.println(person.toString());
+        }
+    }
+
+    public static int calculateSalaries(Employee[] employees) {
+        int sum = 0;
+        for (Employee person : employees) {
+            sum += person.getSalary();
+        }
+        return sum;
+    }
+
+    public static int minSalary(Employee[] employees) {
+        int min = employees[0].getSalary();
+        for (Employee person : employees) {
+            if (person.getSalary() < min) {
+                min = person.getSalary();
+            }
+        }
+        return min;
+    }
+
+    public static int maxSalary(Employee[] employees) {
+        int max = employees[0].getSalary();
+        for (Employee person : employees) {
+            if (person.getSalary() > max) {
+                max = person.getSalary();
+            }
+        }
+        return max;
+    }
+
+    public static int getAverageSalary(Employee[] employees) {
+        return calculateSalaries(employees) / employees.length;
+    }
+
+    public static void printNames(Employee[] employees) {
+        for (Employee person : employees) {
+            System.out.printf("%s %s %s\n", person.getLastName(), person.getFirstName(), person.getMiddleName());
+
+        }
+    }
+
+
 }
